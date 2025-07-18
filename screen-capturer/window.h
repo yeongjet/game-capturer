6f7bc255952c894ebed524735a81304055266c72
yeongjet <yeongjet@gmail.com>
2025-07-18 07:54:42 -0700

    update

--- screen-capturer/window.h
+++ screen-capturer/window.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <cstdint>
+#include <string>
 
 struct Window
 {
     uint8_t id;
-    char* title;
+    std::string title;
     uint16_t width;
-	uint16_t height;
+    uint16_t height;
     size_t pixel_count;
 };
